d66b99dd3929a6142b2476bcd7d75733f265cca0
syedshaneraza <syed.shaneraza@phaedrasolutions.com>
2022-02-22 11:25:32 +0500

    section 4 complete

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,26 +1,18 @@
-import React, {useState} from 'react';
-import ExpenseDate from './ExpenseDate';
-import Card from '../UI/Card';
-import './ExpenseItem.css';
+import React, { useState } from "react";
+import ExpenseDate from "./ExpenseDate";
+import Card from "../UI/Card";
+import "./ExpenseItem.css";
 
-function ExpenseItem(props) {
-  const [title, setTitle] = useState(props.title);
-
-  
-  const clickHandler = function(){
-    setTitle('Updated Title');
-    // console.log(title);
-  }
+const ExpenseItem = function (props) {
   return (
-    <Card className='expense-item'>
+    <Card className="expense-item">
       <ExpenseDate date={props.date} />
-      <div className='expense-item__description'>
-        <h2>{title}</h2>
-        <div className='expense-item__price'>${props.amount}</div>
-        <button onClick={clickHandler}>Change Title</button> 
+      <div className="expense-item__description">
+        <h2>{props.title}</h2>
+        <div className="expense-item__price">${props.amount}</div>
       </div>
     </Card>
   );
-}
+};
 
 export default ExpenseItem;
